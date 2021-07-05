dc0b18e9cb4694c2a98744a88576e71cb13ed7b2
keis8221 <54695442+keis8221@users.noreply.github.com>
2021-07-05 23:51:00 +0900

    動作完璧っす！こうするとコードがまとまってもっと分かりやすくなるよ！

--- test7.py
+++ test7.py
@@ -2,15 +2,22 @@
 
 name=input("名前の入力：")
 k=int(input("欠席回数の入力："))
-
 if k>=5:
     print('\033[31m'+name+'\033[0m')
-
-if 3<=k<5:
+elif 3<=k<5:
     print('\033[33m'+name+'\033[0m')
-
-if k<3:
+else:
     print('\033[37m'+name+'\033[0m')
+
+    
+# if k>=5:
+#     print('\033[31m'+name+'\033[0m')
+
+# if 3<=k<5:
+#     print('\033[33m'+name+'\033[0m')
+
+# if k<3:
+#     print('\033[37m'+name+'\033[0m')
 
 
 
