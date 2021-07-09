f2ff3881e8afd537beaa2db0824ba453c55ce9ce
sage0975 <85611258+sage0975@users.noreply.github.com>
2021-07-09 16:37:06 +0900

    出席管理機能のプログラムです

---
+++ test8.py
@@ -0,0 +1,25 @@
+import linecache
+import pandas as pd
+
+df = pd.read_csv('-F1.csv')
+
+
+target_line = linecache.getline('-F1.csv', 2)
+num = target_line.count('×')
+
+# print(target_line)
+print(num)
+#欠席回数で学生名の色を変える
+name = df["名前"][0]
+k=num
+
+if k>=5:
+    print('\033[31m'+name+'\033[0m')
+
+elif 3<=k<5:
+    print('\033[33m'+name+'\033[0m')
+
+else:
+    print('\033[37m'+name+'\033[0m')
+
+linecache.clearcache() 
