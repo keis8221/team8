ffd40dfe1900604f577b714eb80053e45ce74cf8
sage0975 <85611258+sage0975@users.noreply.github.com>
2021-07-09 22:08:27 +0900

    出席管理機能のプログラム
    学生を二人にしてfor文使ったプログラムです．実行できました！

--- test8.py
+++ test8.py
@@ -3,23 +3,24 @@ import pandas as pd
 
 df = pd.read_csv('-F1.csv')
 
+for i in range(2):
+ target_line = linecache.getline('-F1.csv', i+2)
+ num = target_line.count('×')
 
-target_line = linecache.getline('-F1.csv', 2)
-num = target_line.count('×')
+ # print(target_line) 
+ print(num)
 
-# print(target_line)
-print(num)
-#欠席回数で学生名の色を変える
-name = df["名前"][0]
-k=num
+ #欠席回数で学生名の色を変える
+ name = df["名前"][i]
+ k=num
 
-if k>=5:
-    print('\033[31m'+name+'\033[0m')
+ if k>=5:
+     print('\033[31m'+name+'\033[0m')
 
-elif 3<=k<5:
-    print('\033[33m'+name+'\033[0m')
+ elif 3<=k<5:
+     print('\033[33m'+name+'\033[0m')
 
-else:
-    print('\033[37m'+name+'\033[0m')
+ else:
+     print('\033[37m'+name+'\033[0m')
 
-linecache.clearcache() 
+ linecache.clearcache() 
